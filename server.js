ddc704e3011d4c1d7857ed97e3c2f6e770ddc618
Eric Sowell <eric.sowell@gmail.com>
2017-09-07 09:00:02 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/data', {
+  useMongoClient: true
+})
+mongoose.Promise = global.Promise;
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
+
+// Need to be able to post info
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// These are all the routes for the API.
+app.use(require('./api-routes.js'));
+
+var port = process.env.PORT || 5003;
+
+app.listen(port, function() {
+  console.log('listening on port', port);
+});
